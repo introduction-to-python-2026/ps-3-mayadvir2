7fa235b4bf8ab01e7b08f7db5c0817ca681468d0
mayadvir2 <mayadvir222@gmail.com>
2025-11-06 11:04:30 +0200

    maya 3

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,7 +1,6 @@
 def move(my_list, direction):
     index_of_one = my_list.index(1)
 
-    # Move the one to the left or to the right
     if direction == 'right':
       if index_of_one == (len(my_list)-1):
         return my_list
@@ -17,6 +16,10 @@ def move(my_list, direction):
         my_list[index_of_one - 1] = 1
 
     return my_list
+
+  
+
+
 
   
 
